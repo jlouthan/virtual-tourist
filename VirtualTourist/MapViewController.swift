1e673a1c2857112bf68a4ac5b16b8e813fc2aed9
Jennifer Louthan <jenny@uncorkedstudios.com>
2016-06-04 16:31:35 -0700

    Initial xcode project with a map view

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Jennifer Louthan on 6/4/16.
+//  Copyright © 2016 JennyLouthan. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
